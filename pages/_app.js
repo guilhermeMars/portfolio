4b5da3aebaec0575c9ce22f6a968445b423ff7a8
Guilherme Martins <guilhermemspiandorin@gmail.com>
2022-08-16 16:45:41 -0300

    Title adicionado

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+      <title>Guilherme Mars</title>
+      <meta
+        name="description"
+        content="Desenvolvedor web Front-end especializado em React/Next.js!"
+      />
+      <meta name="author" content="Guilherme Martins" />
+    </Head>
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
